fb29c16788e5e686045eb220667c9a7f2b1d9ac6
Michael Peterson <mpeterson418@gmail.com>
2015-03-03 16:05:20 -0600

    Having no boards shows a message now.

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -5,7 +5,6 @@ angular.module('dashboard', ['boards', 'authentication', 'users'])
 
   boards.all().success(function() {
     for(var i=0; i<$scope.boards.length; i++) {
-      console.log($scope.boards[i].members);
       for(var j=0; j<$scope.boards[i].members.length; j++) {
         member = $scope.boards[i].members[j];
         if(member.email)
